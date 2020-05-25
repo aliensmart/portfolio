148b3083c76a15e5ee77125848ede801a7d92fb1
aliensmart <kaoua17@gmail.com>
2020-05-25 19:22:09 -0400

    added certification in pages folder

---
+++ src/pages/Certifications.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from "../components/Layout";
+import Sectiontitle from "../components/Sectiontitle";
+
+const Certifications = ()=>{
+    return(
+        <Layout>
+            <div className="mi-about mi-section mi-padding-top mi-padding-bottom">
+                    <div className="container">
+                            <Sectiontitle title="Certifications" />
+                            <div className="row mt-30-reverse">
+                                Certifications
+                            </div>
+                    </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Certifications;
